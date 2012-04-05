a550ca4c57d0f37b0d0e0602642fa4ab89af134c
adrianopatrick@gmail.com <adrianopatrick@gmail.com@1e66d768-5b1f-9128-64cc-9d9093521ab4>
2012-04-05 02:01:01 +0000

    iniciado classe que trabalha com leitura e escrita com arquivos, porêm a tarefa ficou com a raquel

---
+++ CaixeiroViajanteGuloso/src/br/uece/caixeiroviajanteguloso/utils/ArquivoUtils.java
@@ -0,0 +1,17 @@
+package br.uece.caixeiroviajanteguloso.utils;
+
+import java.io.File;
+
+public class ArquivoUtils {
+	
+	public static void salvar(String arquivo, String conteudo) {
+		
+		File file = new File(arquivo);
+		
+		if(!file.exists()){
+			
+		}
+		
+	}
+
+}
