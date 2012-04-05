ead0b66ea2a674bd6aa10ad283c685c5aceee10b
adrianopatrick@gmail.com <adrianopatrick@gmail.com@1e66d768-5b1f-9128-64cc-9d9093521ab4>
2012-04-05 03:44:57 +0000

    patrick cunha - criado classes de domínio do problema

---
+++ CaixeiroViajanteGuloso/src/br/uece/caixeiroviajanteguloso/heuristica/MatrizAdjacencias.java
@@ -0,0 +1,42 @@
+package br.uece.caixeiroviajanteguloso.heuristica;
+
+import java.util.List;
+
+import br.uece.caixeiroviajanteguloso.utils.Mathematica;
+
+/**
+ * @author patrick cunha
+ * @version 1.0
+ * Classe responsável por criar a matriz de adjacencia
+ * apartir de um List de Ponto ou de uma vetor com 
+ * valores de metade da matriz
+ * */
+public class MatrizAdjacencias {
+	
+	
+	public Celula[][] geraMatrizAdjacenciasApartirDePontos(List<Ponto> pontos){
+		
+		Celula matriz[][] = new Celula[pontos.size()][pontos.size()];
+		
+		for (int i = 0; i < pontos.size(); i++) {
+			for (int j = 0; j < pontos.size(); j++) {
+				if(i != j)
+					matriz[i][j] = Mathematica.calculaDistancia(pontos.get(i), pontos.get(j));
+				else
+					matriz[i][j] = new Celula(pontos.get(i), pontos.get(j), 0.0);
+			}
+		}
+		
+		return matriz;
+	}
+	
+	public void geraMatrizAdjacenciasApartirDeMatrizIncompleta(Celula[][] matriz){
+		
+		for (int i = 0; i < matriz.length - 1; i++) {
+			for (int j = i + 1; j < matriz.length; j++) {
+				matriz[j][i] = new Celula(matriz[i][j].getDestino(), matriz[i][j].getOrigem(), matriz[i][j].getDistancia());
+			}			
+		}
+	}
+
+}
---
+++ CaixeiroViajanteGuloso/src/br/uece/caixeiroviajanteguloso/heuristica/Ponto.java
@@ -0,0 +1,73 @@
+package br.uece.caixeiroviajanteguloso.heuristica;
+
+/**
+ * @author patrick cunha
+ * @version 1.0 
+ * */
+public class Ponto {
+	
+	private Integer id;
+	private Float coordX;
+	private Float coordY;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Float getCoordX() {
+		return coordX;
+	}
+	public void setCoordX(Float coordX) {
+		this.coordX = coordX;
+	}
+	public Float getCoordY() {
+		return coordY;
+	}
+	public void setCoordY(Float coordY) {
+		this.coordY = coordY;
+	}	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((coordX == null) ? 0 : coordX.hashCode());
+		result = prime * result + ((coordY == null) ? 0 : coordY.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ponto other = (Ponto) obj;
+		if (coordX == null) {
+			if (other.coordX != null)
+				return false;
+		} else if (!coordX.equals(other.coordX))
+			return false;
+		if (coordY == null) {
+			if (other.coordY != null)
+				return false;
+		} else if (!coordY.equals(other.coordY))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Ponto [id=" + id + ", coordX=" + coordX + ", coordY=" + coordY
+				+ "]";
+	}	
+	
+}
