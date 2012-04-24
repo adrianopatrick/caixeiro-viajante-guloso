36bd4f04d02ec2bf3ad6b0a43e96f1659259a9c1
adrianopatrick@gmail.com <adrianopatrick@gmail.com@1e66d768-5b1f-9128-64cc-9d9093521ab4>
2012-04-24 01:18:41 +0000

    patrick cunha - criado branch com melhor caminho[buggado]

---
+++ src/br/uece/caixeiroviajanteguloso/heuristica/Main.java
@@ -0,0 +1,12 @@
+package br.uece.caixeiroviajanteguloso.heuristica;
+
+import br.uece.caixeiroviajanteguloso.ui.frmPrincipal;
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		frmPrincipal form = new frmPrincipal();
+		form.setVisible(true);
+	}
+}
